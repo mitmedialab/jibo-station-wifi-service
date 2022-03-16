9156ebdc777b455951e0bf990fe9a2e9d7403031
Jon Ferguson <jon@jef.us>
2022-03-16 13:38:44 -0400

    installed virtualize

--- install.sh
+++ install.sh
@@ -1,55 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-VIRTUALENV_ROOT=`pwd`
-export VIRTUAL_ENV="`basename $VIRTUALENV_ROOT`"
-export PATH=$VIRTUALENV_ROOT/bin:$VIRTUALENV_ROOT/node/bin:$PATH
-export N_PREFIX=$VIRTUALENV_ROOT/node
+# install git submodules
+git submodule init
+git submodule update
+virtualize/install.sh
 
-NODE_VERSION=16.13.1
+### as you add virtualize modules (node, python, etc...) you will
+### probably want to edit the following sections and enable them
 
-if [ "$VIRTUALENV_ROOT" != "/usr/local/jibo-station-wifi-service" ]; then
-    echo "error: this needs to be installed into and run from /usr/local/jibo-station-wifi-service"
-    exit 1
-fi
+### node
+# yarn install
 
-if [ ! -d node ]; then
-    mkdir node
-    git clone https://github.com/tj/n node/n
-    (export PREFIX=`pwd`/node; cd node/n; make; make install)
-    N_PREFIX=`pwd`/node node/bin/n $NODE_VERSION
-    npm install -g yarn
-fi
-
-if [ -e node_modules ]; then
-    echo "removing and reinstalling node modules"
-    rm -rf node_modules
-fi
-yarn install
-
-sudo cp -p jibo-station-wifi-service.service /etc/systemd/system
-sudo systemctl enable jibo-station-wifi-service.service
-
-if egrep "^/usr/local/jibo-station-wifi-service/" /etc/rc.local; then
-    echo "disabling launching from /etc/rc.local"
-    sudo sed -ie "s%^/usr/local/jibo-station-wifi-service/%#/usr/local/jibo-station-wifi-service/%" /etc/rc.local
-fi
-
-if [ ! -e /var/run/wpa_supplicant ]; then
-    echo "don't forget to add ctrl_interface=/var/run/wpa_supplicant"
-    echo "as the first line in /etc/wpa_supplicant.conf!"
-fi
-
-echo ""
-echo "done!"
-
-read -p "reboot [y]? " yn
-case $yn in
-    ""|[yY]|yes|Yes|YES )
-	sudo reboot
-	;;
-    * )
-	echo "not rebooting"
-	;;
-esac
-
-exit
+### python
+# pip install -f requirments.txt
