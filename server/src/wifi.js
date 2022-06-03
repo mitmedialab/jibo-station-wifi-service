ae1a27cf80578be0162f39ced99b3b5b1d347b37
Jon Ferguson <jon@media.mit.edu>
2022-06-03 13:53:00 -0400

    yet again fixed the wireless scanning in the lab. this time it was overrunning the stdout buffer on the exec command

--- server/src/wifi.js
+++ server/src/wifi.js
@@ -5,7 +5,7 @@ const fsP = require('fs').promises;
 const util = require('util');
 const child_process = require('child_process');
 const { Wireless, Monitor } = require('wirelesser');
-const iw = require('wireless-tools/iw');
+const iw = require('./iw');
 const isOnline = require('is-online');
 const isTcpOn = require('is-tcp-on');
 const roslib = require('roslib');
@@ -165,7 +165,6 @@ class WiFi {
 		    console.log('initiating scan');
                     //this.wireless.scan()
                     let scan = util.promisify((iface, callback) => iw.scan(iface, callback));
-                    //this.wireless.scan()
                     scan(INTERFACE)
                         .then(networks => {
                             console.log(`scan finished. found ${networks.length} wireless networks at ${INTERFACE}`);
